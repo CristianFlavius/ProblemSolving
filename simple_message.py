08c63c1ce4ab959dcc5b4279af070659350246a5
CristianFlavius <cristianraduflavius@gmail.com>
2023-04-26 13:25:30 +0100

    Updated Simple Messages

--- simple_message.py
+++ simple_message.py
@@ -2,3 +2,9 @@
 
 print("Simple Text Output")
 print("Second Line Of Text")
+
+# Simple Input gathering from Person
+
+print("What do you wanna print?")
+letter = input("Letter? : ")
+print("You Wrote : " + letter)
